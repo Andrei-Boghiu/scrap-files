05b432e268a02590123b5ca46e883a9f22d7aee3
Andrew <124816994+Andrei-Boghiu@users.noreply.github.com>
2023-04-17 14:59:48 +0300

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -68,3 +68,22 @@ while (count < 100) {
     fizzBuzz();   
 }
 // -------Third Try---------
+
+// -------Fourth Try--------
+var output = [];
+
+function fizzBuzz() {
+    for (var count = 1; count < 101; count++) {
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push("fizzBuzz");
+        } else if (count % 3 === 0) {
+            output.push("fizz");
+        } else if (count % 5 === 0) {
+            output.push("buzz");
+        } else {
+            output.push(count);        
+        }
+    }
+    return output
+}
+// -------Fourth Try--------
