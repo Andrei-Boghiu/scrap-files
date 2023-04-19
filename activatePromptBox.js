e6167e427bb98159f26eaae5d7fb15c099e13cab
Andrew <124816994+Andrei-Boghiu@users.noreply.github.com>
2023-04-19 14:52:09 +0300

    Update activatePromptBox.js

--- activatePromptBox.js
+++ activatePromptBox.js
@@ -1,7 +1,21 @@
 function activatePromptBox() {
-    const commentInputElement = document.getElementsByClassName("comments")[0].children[0];
-    const userComment = prompt("Please add comment!");
+    const userComment = prompt("Please add info!");
 
+    const commentInputElement = document.getElementsByClassName("comments-input")[0].children[0];
     commentInputElement.value = userComment;
     commentInputElement.dispatchEvent(new Event('change'));
+
+    const commentLength = commentInputElement.value.length;
+    const saveButton = document.querySelector("button");
+
+    setTimeout(function () {
+        if (commentLength > 1) {
+            //saveButton.click()
+            saveButton.dispatchEvent(new Event("click"));
+            saveButton.style.color = 'green'
+            setTimeout(function () {
+                saveButton.style.color = ""
+            }, 5000)
+        }
+    }, 500)
 }
